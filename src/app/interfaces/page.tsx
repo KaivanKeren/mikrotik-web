0fd0bd1c46df727099bdcd7c67bcb858a5ac01d4
Ismail <x.iml.7122@gmail.com>
2025-02-09 16:19:15 +0700

    Add sidebar and interface components; update package dependencies

---
+++ src/app/interfaces/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import InterfaceComponent from '@/components/InterfaceComponent'
+import Sidebar from '@/components/Sidebar'
+import React, { useState } from 'react'
+
+const InterfacePage = () => {
+        const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+  return (
+    <div className="flex">
+      <Sidebar
+        collapsed={sidebarCollapsed}
+        setCollapsed={setSidebarCollapsed}
+      />
+      <div className="flex-1 min-h-screen bg-gray-50 p-8">
+        <InterfaceComponent />
+      </div>
+      </div>
+  )
+}
+
+export default InterfacePage
