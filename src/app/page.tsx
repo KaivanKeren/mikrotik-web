546eb432a0b51feed6ba1c8fb22b362a1faad850
Ismail <x.iml.7122@gmail.com>
2025-02-09 16:26:38 +0700

    Add sidebar component and implement responsive layout in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+
 import NetworkDashboard from "@/components/NetworkDashboard";
+import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
 
 export default function Home() {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   return (
-   <NetworkDashboard />
+    <div className="flex">
+      <Sidebar
+        collapsed={sidebarCollapsed}
+        setCollapsed={setSidebarCollapsed}
+      />
+      <div className="flex-1 min-h-screen bg-gray-50 p-8">
+        <NetworkDashboard />
+      </div>
+    </div>
   );
 }
