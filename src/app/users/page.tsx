24ac07f156e1709428e169a157f8544c87042324
Ismail <x.iml.7122@gmail.com>
2025-02-22 07:43:45 +0700

    Add Sidebar component to UsersPage and implement sidebar collapse functionality

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+import Sidebar from "@/components/Sidebar";
+import React, { useState } from "react";
+
+export default function UsersPage() {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 
   return (
-    <div>UsersPage</div>
-  )
+    <div className="flex">
+      <Sidebar
+        collapsed={sidebarCollapsed}
+        setCollapsed={setSidebarCollapsed}
+      />
+      <div>UsersPage</div>
+    </div>
+  );
 }
